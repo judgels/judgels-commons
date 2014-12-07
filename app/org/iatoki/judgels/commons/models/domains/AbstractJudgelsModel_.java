0cbd5b94c64b5263e0846c284c50cf4d90004b70
Jordan Fernando <fernandojordan.92@gmail.com>
2014-12-07 18:23:29 +0700

    Commit updated metamodel for commons

---
+++ app/org/iatoki/judgels/commons/models/domains/AbstractJudgelsModel_.java
@@ -0,0 +1,15 @@
+package org.iatoki.judgels.commons.models.domains;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(AbstractJudgelsModel.class)
+public abstract class AbstractJudgelsModel_ extends AbstractModel_ {
+
+	public static volatile SingularAttribute<AbstractJudgelsModel, Long> id;
+	public static volatile SingularAttribute<AbstractJudgelsModel, String> jid;
+
+}
+
