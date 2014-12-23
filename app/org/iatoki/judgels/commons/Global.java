ff03a49b03da70e7191547acf9bb3a6c4fee4402
Jordan Fernando <fernandojordan.92@gmail.com>
2014-12-23 18:23:29 +0700

    Disable csrffilter for now

--- app/org/iatoki/judgels/commons/Global.java
+++ app/org/iatoki/judgels/commons/Global.java
@@ -2,7 +2,6 @@ package org.iatoki.judgels.commons;
 
 import play.GlobalSettings;
 import play.api.mvc.EssentialFilter;
-import play.filters.csrf.CSRFFilter;
 import play.filters.gzip.GzipFilter;
 
 public abstract class Global extends GlobalSettings {
@@ -10,6 +9,6 @@ public abstract class Global extends GlobalSettings {
     @Override
     @SuppressWarnings("unchecked")
     public <T extends EssentialFilter> Class<T>[] filters() {
-        return new Class[]{CSRFFilter.class, GzipFilter.class};
+        return new Class[]{GzipFilter.class};
     }
 }
