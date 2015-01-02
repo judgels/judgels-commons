d34e0576b1209f9ba051c824a5b96d56d2f51490
Ashar Fuadi <fushar@gmail.com>
2015-01-02 16:56:56 +0700

    Add abstraction to file system I/O

---
+++ app/org/iatoki/judgels/commons/FileSystemProvider.java
@@ -0,0 +1,25 @@
+package org.iatoki.judgels.commons;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.List;
+
+public interface FileSystemProvider {
+    void createDirectory(List<String> directoryPath);
+
+    void createFile(List<String> filePath);
+
+    void writeToFile(List<String> filePath, String content);
+
+    String readFromFile(List<String> filePath);
+
+    void uploadFile(List<String> destinationDirectoryPath, File file, String destinationFilename);
+
+    void uploadZippedFiles(List<String> destinationDirectoryPath, File zippedFiles);
+
+    ByteArrayOutputStream getZippedFilesInDirectory(List<String> directoryPath);
+
+    List<FileInfo> listFilesInDirectory(List<String> directoryPath);
+
+    String getURL(List<String> filePath);
+}
