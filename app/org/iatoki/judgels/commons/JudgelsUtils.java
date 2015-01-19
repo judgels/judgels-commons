a6fa400be4b3e225f50032d4bc5242bac7817d37
Ashar Fuadi <fushar@gmail.com>
2015-01-19 16:56:56 +0700

    Tidy up layouts

--- app/org/iatoki/judgels/commons/JudgelsUtils.java
+++ app/org/iatoki/judgels/commons/JudgelsUtils.java
@@ -2,11 +2,13 @@ package org.iatoki.judgels.commons;
 
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringEscapeUtils;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 public final class JudgelsUtils {
@@ -15,12 +17,16 @@ public final class JudgelsUtils {
         // prevents instantiation
     }
 
-    public static String timestampToFormattedDate(long timestamp) {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd - HH:mm");
+    public static String formatDate(Date date) {
+        return formatDate(date.getTime());
+    }
+
+    public static String formatDate(long timestamp) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy HH:mm");
         return formatter.format(timestamp);
     }
 
-    public static String byteCountToFormattedSize(long bytes) {
+    public static String formatBytesCount(long bytes) {
         return FileUtils.byteCountToDisplaySize(bytes);
     }
 
@@ -28,13 +34,16 @@ public final class JudgelsUtils {
         return messageDigest(UUID.randomUUID().toString(), "MD5");
     }
 
-
     public static String hashSHA256(String s) {
         return messageDigest(s, "SHA-256");
     }
 
     public static String hashMD5(String s) {
         return messageDigest(s, "MD5");
+    }
+
+    public static String escapeHtmlString(String string) {
+        return StringEscapeUtils.escapeHtml4(string).replaceAll("\r\n", "<br />");
     }
 
     private static String messageDigest(String s, String algorithm) {
