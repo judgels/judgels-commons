714562fa2c29258b9e194a1c6d090657ed4ab5c3
Jordan Fernando <fernandojordan.92@gmail.com>
2015-04-07 05:59:59 +0700

    Add Guard for Unsupported Operation Exception

--- app/org/iatoki/judgels/commons/controllers/BaseController.java
+++ app/org/iatoki/judgels/commons/controllers/BaseController.java
@@ -3,6 +3,7 @@ package org.iatoki.judgels.commons.controllers;
 import play.mvc.Controller;
 
 @EntityNotFoundGuard
+@UnsupportedOperationGuard
 public abstract class BaseController extends Controller {
 
 }
