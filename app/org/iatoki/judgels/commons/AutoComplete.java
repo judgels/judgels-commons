7ec5c3029bf50156a8f3e89cae454b565d987311
Jordan Fernando <fernandojordan.92@gmail.com>
2015-04-13 15:56:30 +0700

    Move autocomplete from Jophiel into play-commons

---
+++ app/org/iatoki/judgels/commons/AutoComplete.java
@@ -0,0 +1,28 @@
+package org.iatoki.judgels.commons;
+
+public final class AutoComplete {
+
+    private final String id;
+
+    private final String value;
+
+    private final String label;
+
+    public AutoComplete(String id, String value, String label) {
+        this.id = id;
+        this.value = value;
+        this.label = label;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
