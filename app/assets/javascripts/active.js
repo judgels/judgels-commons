be8bc94c2be85896918c243f42379c73c66b3ba6
Jordan Fernando <fernandojordan.92@gmail.com>
2015-05-10 21:57:05 +0700

    Support better view when on zoomed browser or mobile, run some scripts without document ready for faster view
    resolve ia-toki/judgels-uriel#4

--- app/assets/javascripts/active.js
+++ app/assets/javascripts/active.js
@@ -1,16 +1,14 @@
 require(["jquery", "bootstrap"], function( __jquery__ ) {
-    $(document).ready(function() {
-        $($(".breadcrumb-link").get().reverse()).each(function() {
-            var href = $(this).attr("href");
-            $("a[href='" + href +"']").each(function() {
-                var parent = $(this).parent();
-                if (parent.siblings(".active").size() === 0) {
-                    parent.addClass("active");
-                    if (parent.is("li")) {
-                        parent.parentsUntil("body", "li").addClass("active");
-                    }
+    $($(".breadcrumb-link").get().reverse()).each(function() {
+        var href = $(this).attr("href");
+        $("a[href='" + href +"']").each(function() {
+            var parent = $(this).parent();
+            if (parent.siblings(".active").size() === 0) {
+                parent.addClass("active");
+                if (parent.is("li")) {
+                    parent.parentsUntil("body", "li").addClass("active");
                 }
-            });
+            }
         });
     });
 });
--- app/assets/javascripts/language.js
+++ app/assets/javascripts/language.js
@@ -1,7 +1,5 @@
 require(["jquery", "bootstrap", "bootstrap-select"], function( __jquery__ ) {
-    $(document).ready(function() {
-        $("#languages").change(function() {
-            window.location = $("#languages").val();
-        });
+    $("#languages").change(function() {
+        window.location = $("#languages").val();
     });
 });
