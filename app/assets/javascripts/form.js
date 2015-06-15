bf9c4dda150f211ef797c916af25bcfc3feadb7e
Jordan Fernando <fernandojordan.92@gmail.com>
2015-06-15 15:21:17 +0700

    Add function to uncheck radio input option
    resolve ia-toki/judgels#206

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,5 +1,21 @@
 require(["jquery"], function( __jquery__ ) {
     $(document).ready(function() {
+        $("input[type='radio']").click(function(e) {
+            var that = $(this);
+            var checked = $("input[type='radio'][name='" + that.attr("name") + "']:checked");
+            if (checked.hasClass('singleCheck')) {
+                checked.attr("checked", false);
+                checked.removeClass("singleCheck");
+            }
+        });
+
+        $("input[type='radio']").change(function(e) {
+            var that = $(this);
+            var all = $("input[type='radio'][name='" + that.attr("name") + "']");
+            all.removeClass("singleCheck");
+            that.addClass("singleCheck");
+        });
+
         $("form").submit(function(e) {
             var $form = $(this);
 
