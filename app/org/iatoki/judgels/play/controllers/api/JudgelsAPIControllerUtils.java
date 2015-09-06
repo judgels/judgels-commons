78c30e0beeca315163042f50e9587f79c9dc2810
Ashar Fuadi <fushar@gmail.com>
2015-09-06 20:13:55 +0700

    Change AbstractJudgelsAPIController into util class

---
+++ app/org/iatoki/judgels/play/controllers/api/JudgelsAPIControllerUtils.java
@@ -0,0 +1,20 @@
+package org.iatoki.judgels.play.controllers.api;
+
+import org.apache.commons.lang3.StringUtils;
+import play.mvc.Http;
+
+public final class JudgelsAPIControllerUtils implements Http.HeaderNames {
+
+    public static void setAccessControlOrigin(String domains, String methods, long maxAge) {
+        Http.Context.current().response().setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, domains);
+        Http.Context.current().response().setHeader(ACCESS_CONTROL_ALLOW_METHODS, methods);
+        Http.Context.current().response().setHeader(ACCESS_CONTROL_MAX_AGE, maxAge + "");
+        Http.Context.current().response().setHeader(ACCESS_CONTROL_ALLOW_HEADERS, StringUtils.join(new String[] {ORIGIN, X_REQUESTED_WITH, CONTENT_TYPE, ACCEPT, AUTHORIZATION}, ','));
+    }
+
+    public static String createJsonPResponse(String callback, String json) {
+        StringBuilder sb = new StringBuilder(callback);
+        sb.append("(").append(json).append(")");
+        return sb.toString();
+    }
+}
