45639b2f4155fecbcfa75699be1828d51baa33af
Jordan Fernando <fernandojordan.92@gmail.com>
2015-10-02 14:56:55 +0700

    Change display time relative to server time

--- app/assets/javascripts/server-time.js
+++ app/assets/javascripts/server-time.js
@@ -12,4 +12,17 @@ require(["jquery", "jquery-timer", "moment"], function() {
     $(document).ready(updateServerTime);
     $(document).stopTime("server-time");
     $(document).everyTime("1s", "server-time", updateServerTime);
+
+    var updateDisplayTime = function() {
+        var elements = $("time.display-time");
+        for (var i = 0; i < elements.length; i++) {
+            var element = $(elements[i]);
+            var datetime = moment(element.attr("datetime"));
+            element.attr("title", datetime.format("DD-MMM-YYYY HH:mm:ss Z"));
+            element.text(datetime.from(serverDateTime));
+        }
+    };
+    $(document).ready(updateDisplayTime);
+    $(document).stopTime("display-time");
+    $(document).everyTime("60s", "display-time", updateDisplayTime);
 });
