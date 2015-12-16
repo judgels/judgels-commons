11001389731abe8761489e8ff871b89c24ac8b31
Ashar Fuadi <fushar@gmail.com>
2015-12-15 21:54:15 -0500

    Refactor assets initialization into an init module

--- app/org/iatoki/judgels/play/JudgelsPlayCommonsModule.java
+++ app/org/iatoki/judgels/play/JudgelsPlayCommonsModule.java
@@ -2,6 +2,7 @@ package org.iatoki.judgels.play;
 
 import com.google.inject.AbstractModule;
 import org.iatoki.judgels.Config;
+import org.iatoki.judgels.play.asset.LocalAssetsInit;
 import org.iatoki.judgels.play.general.GeneralConfigSource;
 
 public final class JudgelsPlayCommonsModule extends AbstractModule {
@@ -9,5 +10,7 @@ public final class JudgelsPlayCommonsModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(Config.class).annotatedWith(GeneralConfigSource.class).toInstance(ApplicationConfig.getInstance());
+
+        bind(LocalAssetsInit.class).asEagerSingleton();
     }
 }
